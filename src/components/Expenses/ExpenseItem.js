7b4ba8b7ae49e40e236bbcc9ab1214838cb1c320
cupid chakma <greendaycu20@gmail.com>
2022-07-02 13:09:51 +0600

    updated expense-item.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,15 @@ import Card from '../UI/Card';
 
 function ExpenseItem(props) {
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.expenseDate}/>
-            <div className='expense-item__description'>
-                <h2>{props.expenseTitle}</h2>
-                <div className='expense-item__price'>Amount : ${props.amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.expenseDate}/>
+                <div className='expense-item__description'>
+                    <h2>{props.expenseTitle}</h2>
+                    <div className='expense-item__price'>Amount : ${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
