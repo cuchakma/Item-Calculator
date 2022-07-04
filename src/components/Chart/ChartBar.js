6a431bdfbaedd8346c863ea7a0239183f878db06
cupid chakma <greendaycu20@gmail.com>
2022-07-04 15:07:21 +0600

    chartbar.js updated

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './ChartBar.css';
 
 const ChartBar = (props) => {
     let barFillHeight = '0%';
-    console.log(props);
+
     if( props.maxValue > 0 ) {
         barFillHeight = Math.round(props.value / props.maxValue * 100) + '%';
     }
